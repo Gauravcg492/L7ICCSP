0df04fe6e558cd801c0c433e347b3f8d27a14714
Siva Girish <sivagirish81@gmail.com>
2021-02-03 22:30:16 +0530

    Sha256 File utility added

--- Capstone Phase 2/L7ICCSP/src/main/code/Utils/hashes.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/Utils/hashes.ts
@@ -1,5 +1,8 @@
 import CryptoJS from 'crypto-js';
+import * as fs from "fs";
 
-module.exports = function sha256(data : string){
-    return CryptoJS.HmacSHA1(data,"MyKEY");
+module.exports = function sha256(File : string){
+    const data = fs.readFileSync(File,"utf-8");
+    return CryptoJS.SHA256(data);
 }
+
