9060f9c9636fcf44f826f50fdc5f2e1fb5616382
Sangam Kedilaya <skedilay@akamai.com>
2021-02-04 23:45:49 +0530

    Sha256 file utility extended

--- Capstone Phase 2/L7ICCSP/src/main/code/Utils/hashes.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/Utils/hashes.ts
@@ -1,8 +1,24 @@
 import CryptoJS from 'crypto-js';
 import * as fs from "fs";
 
-module.exports = function sha256(File : string){
+export function sha256(File : string){
     const data = fs.readFileSync(File,"utf-8");
-    return CryptoJS.SHA256(data);
+    console.log(data);
+    return CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
 }
 
+//console.log("starting")
+// console.log(sha256('/Users/skedilay/Desktop/test.txt'))
+
+export function concat(s1: string, s2: string){
+    var s;
+    if(s1.localeCompare(s2)){
+        s = s2.concat(s1.toString());
+    }
+    else{
+        s = s1.concat(s2.toString());
+    }
+    return CryptoJS.SHA256(s).toString(CryptoJS.enc.Hex);
+}
+
+//console.log(concat("san", "ked"))
