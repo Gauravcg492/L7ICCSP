7e5722518a456f3b38b8d557ec67ae56df7285c4
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-11 23:36:47 +0530

    Old implementation, commented for reference

--- Capstone Phase 2/L7ICCSP/src/main/code/tree/merkle_tree.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/tree/merkle_tree.ts
@@ -1,99 +1,99 @@
-import { Children } from 'react';
-import { Tree, Node } from './tree';
+// import { Children } from 'react';
+// import { Tree, Node } from './tree';
 
-class MerkleTree implements Tree {
-    private childParentMap: { [key: string]: string };
-    private hashToNode: { [key: string]: Node };
-    private hashesToEdit: string[];
-    private hashesToRemove: string[];
-    private root: Node;
-    private LEAF = 1;
+// class MerkleTree implements Tree {
+//     private childParentMap: { [key: string]: string };
+//     private hashToNode: { [key: string]: Node };
+//     private hashesToEdit: string[];
+//     private hashesToRemove: string[];
+//     private root: Node;
+//     private LEAF = 1;
 
-    constructor(hashes: string[]) {
-        // initialize variables
-        this.hashToNode = {};
-        this.childParentMap = {};
-        this.hashesToEdit = [];
-        this.hashesToRemove = [];
-        this.root = this.getNode("");
-        this.createTree(hashes);
-    }
+//     constructor(hashes: string[]) {
+//         // initialize variables
+//         this.hashToNode = {};
+//         this.childParentMap = {};
+//         this.hashesToEdit = [];
+//         this.hashesToRemove = [];
+//         this.root = this.getNode("");
+//         this.createTree(hashes);
+//     }
 
-    private createTree(this: MerkleTree, hashes: string[]): void {
-        // hash will in the form childHash$parentHash$currentPosition$realPosition
-        for (const hash of hashes) {
-            const hashArr = hash.split('$');
+//     private createTree(this: MerkleTree, hashes: string[]): void {
+//         // hash will in the form childHash$parentHash$currentPosition$realPosition
+//         for (const hash of hashes) {
+//             const hashArr = hash.split('$');
 
-            const childNode = this.getNode(hashArr[0]);
-            childNode.currentPosition = parseInt(hashArr[2]);
-            childNode.realPosition = parseInt(hashArr[3]);
-            this.hashToNode[hashArr[0]] = childNode;
+//             const childNode = this.getNode(hashArr[0]);
+//             childNode.currentPosition = parseInt(hashArr[2]);
+//             childNode.realPosition = parseInt(hashArr[3]);
+//             this.hashToNode[hashArr[0]] = childNode;
 
-            this.childParentMap[hashArr[0]] = hashArr[1];
+//             this.childParentMap[hashArr[0]] = hashArr[1];
 
-            // check if the current node is root
-            if (hashArr[0] === hashArr[1]) {
-                this.root = childNode;
-            } else {
-                const parentNode = this.getNode(hashArr[1]);
-                parentNode.children?.push(childNode);
-                this.hashToNode[hashArr[1]] = parentNode;
-            }
-        }
-    }
+//             // check if the current node is root
+//             if (hashArr[0] === hashArr[1]) {
+//                 this.root = childNode;
+//             } else {
+//                 const parentNode = this.getNode(hashArr[1]);
+//                 parentNode.children?.push(childNode);
+//                 this.hashToNode[hashArr[1]] = parentNode;
+//             }
+//         }
+//     }
 
-    private getNode(this: MerkleTree, hash: string): Node {
-        if (!(hash in this.hashToNode)) {
-            return {
-                hash: hash
-            }
-        }
-        return this.hashToNode[hash];
-    }
+//     private getNode(this: MerkleTree, hash: string): Node {
+//         if (!(hash in this.hashToNode)) {
+//             return {
+//                 hash: hash
+//             }
+//         }
+//         return this.hashToNode[hash];
+//     }
 
-    private find(this: MerkleTree): Node {
-        const stack = [];
-        if (this.root.hash === "") {
-            return this.getNode("");
-        }
-        // search tree for improper node
-        let current = this.root;
-        while (true) {
-            if ("children" in current) {
-                
-            }
-        }
+//     private find(this: MerkleTree): Node {
+//         const stack = [];
+//         if (this.root.hash === "") {
+//             return this.getNode("");
+//         }
+//         // search tree for improper node
+//         let current = this.root;
+//         while ("children" in current && current.children !== undefined) {
+//             if (current.children[0].currentPosition !== current.children[0].realPosition) {
+//                 return current.children[0];
+//             } 
+//             if (current.children[1].currentPosition !== current.children[1].realPosition) {
+//                 return current.children[1];
+//             }
+//         }
+//     }
 
-        
-        
-    }
+//     add(this: MerkleTree, filePath: string): string {
+//         // throw new Error('Method not implemented.');
+//         // TODO call util generateHash util function
+//         // const hash = gethash(filePath);
+//         const hash = filePath;
 
-    add(this: MerkleTree, filePath: string): string {
-        // throw new Error('Method not implemented.');
-        // TODO call util generateHash util function
-        // const hash = gethash(filePath);
-        const hash = filePath;
+//         if (this.root.hash === "") {
+//             // Create the leaf/root node
+//             const newNode = this.getNode(hash);
+//             newNode.currentPosition = this.LEAF;
+//             newNode.realPosition = this.LEAF;
 
-        if (this.root.hash === "") {
-            // Create the leaf/root node
-            const newNode = this.getNode(hash);
-            newNode.currentPosition = this.LEAF;
-            newNode.realPosition = this.LEAF;
+//             // add hash to editList
+//             this.hashesToEdit.push(hash);
+//             return hash;
+//         }
+//         // find appropriate node hash with the new node
 
-            // add hash to editList
-            this.hashesToEdit.push(hash);
-            return hash;
-        }
-        // find appropriate node hash with the new node
+//         return "";
+//     }
 
-        return "";
-    }
+//     delete(this: MerkleTree, filePath: string): string {
+//         throw new Error('Method not implemented.');
+//     }
 
-    delete(this: MerkleTree, filePath: string): string {
-        throw new Error('Method not implemented.');
-    }
-
-    update(this: MerkleTree, filePath: string): string {
-        throw new Error('Method not implemented.');
-    }
-}
+//     update(this: MerkleTree, filePath: string): string {
+//         throw new Error('Method not implemented.');
+//     }
+// }
