a8eaca09a6bcd82550bde49e2cd998ae24caee2c
Siva Girish <sivagirish81@gmail.com>
2021-02-16 22:15:07 +0530

    Verify and Node Parser init

---
+++ Capstone Phase 2/L7ICCSP/src/main/code/Utils/nodeParser.ts
@@ -0,0 +1,7 @@
+
+export function parseNode(Filename : String){
+    // General Structure Of a file name
+        // Filename : node_hash,parent_node_hash,left_child_hash,right_child_hash,position_1,position_2
+    // return as an array
+    
+}
