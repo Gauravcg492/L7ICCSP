b9fa3f56a4d767aa2a3f9cdddf51028b1a9635b9
Sangam Kedilaya <skedilay@akamai.com>
2021-02-16 22:29:12 +0530

    added nodeParser

--- Capstone Phase 2/L7ICCSP/src/main/code/Utils/nodeParser.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/Utils/nodeParser.ts
@@ -1,7 +1,9 @@
 
-export function parseNode(Filename : String){
+export function parseNode(filename : String){
     // General Structure Of a file name
-        // Filename : node_hash,parent_node_hash,left_child_hash,right_child_hash,position_1,position_2
+    // Filename : node_hash,parent_node_hash,left_child_hash,right_child_hash,position_1,position_2
     // return as an array
-    
+    var str = filename; 
+    var splitted = str.split(",", 6); 
+    console.log(splitted);
 }
