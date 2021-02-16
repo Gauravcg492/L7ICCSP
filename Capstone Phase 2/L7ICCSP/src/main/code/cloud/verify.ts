e2825f86fe6b07c862ada7c5f40997db3c8dea66
Siva Girish <sivagirish81@gmail.com>
2021-02-16 23:36:40 +0530

    Verify api integreation & Testing required

--- Capstone Phase 2/L7ICCSP/src/main/code/cloud/verify.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cloud/verify.ts
@@ -2,13 +2,21 @@
 import {parseNode} from '../utils/nodeParser'
 import { sha256 , concat} from '../utils/hashes';
 
-export function verify(filename : string,hash_file : string){
-    var filename_arr = parseNode(filename);
+// Needs to be implemented
+function search(temp : string){         // Needs to be removed - Temp fix
+    return "Temp string";
+}
+
+export function verify(hash_file : string,local_root_hash : string){
+
+    // TODO : Can be a list or just a string
+    // let filename_arr = parseNode(search(hash_file));
+    let filename_arr = ['a','b','c','d','e','f'];   // Needs to be replaces
     // Retrieve Parent node details
-    var tmp;
-    var sibling;
-    var curr_node = hash_file;
-    var tmp_hash;
+    let tmp;
+    let sibling;
+    let curr_node = hash_file;
+    
     while (filename_arr[0] != filename_arr[1]){
         // Call to drive-api : Search
         tmp = parseNode(search(filename_arr[1]));
@@ -20,4 +28,13 @@ export function verify(filename : string,hash_file : string){
         filename_arr = parseNode(search(tmp[1]));
     }
     // compare curr_node with local root
+    // curr_node contains computed root hash 
+    // Needs to be compared to local copy of rootash
+
+    // True indicates - Not Tampered
+    // False indicates - Tampered
+    if (curr_node === local_root_hash)
+        return true;
+    return false;
+    
 }
--- Capstone Phase 2/L7ICCSP/src/main/code/cloud_operations.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cloud_operations.ts
@@ -1,3 +1,6 @@
+import { verify } from './cloud/verify'
+import { AccessStorage } from "./storage/access_storage"
+import { sha256 } from "./utils/hashes"
 // Class which handles upload and download
 class CloudOperations{
     //Return types can be further refined
@@ -5,10 +8,17 @@ class CloudOperations{
     {
 
     }
-    private verify(): Boolean
+    
+    private verify(storage : AccessStorage,filehash : string): Boolean
     {
-        return true;
+        const obj = {
+                 id: "id2",
+                 "configPath": 'config.json'
+         };
+        const root_hash = storage.getRootHash(obj);
+        return verify(filehash,root_hash);
     }
+
     upload(): void
     {
 
