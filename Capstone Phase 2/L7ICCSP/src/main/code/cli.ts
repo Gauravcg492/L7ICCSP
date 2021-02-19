79fa411e14d18697daf00accc12e8c10be75b0bb
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-20 00:14:56 +0530

    template cli added

--- Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
@@ -1,7 +1,24 @@
 import { GoogleAuth } from "./authenticator/googleAuth"
+import readline from 'readline'
 import { GoogleAccessCloud } from "./cloud/google_access_cloud"
 
-let tester = new GoogleAuth();
-//setTimeout(function(){ alert("Hello") },15000);
-//let downloader = new GoogleAccessCloud(tester.getDrive());
-//downloader.getFile("temp");
+const main = async () => {
+    let tester = new GoogleAuth();
+    //setTimeout(function(){ alert("Hello") },15000);
+    //let downloader = new GoogleAccessCloud(tester.getDrive());
+    //downloader.getFile("temp");
+    await tester.authorize();
+    if(!tester.isToken()) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        const it = rl[Symbol.asyncIterator]();
+        console.log("Access this url to get required code: ", tester.getAuthUrl());
+        const code = await it.next();
+        console.log(code)
+        tester.getAccessToken(code as unknown as string);
+    }
+}
+
+main();
