2353ad16d873c2dc96f1eaeaee27df82442ff5ab
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-22 01:46:23 +0530

    added additional method to fetch user details

--- Capstone Phase 2/L7ICCSP/src/main/code/authenticator/authentication.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/authenticator/authentication.ts
@@ -2,4 +2,5 @@
 export interface Authentication{
     authorize(): Promise<void>; // auth function to get login credentials and provide the access token
     getAccessToken(code: string): Promise<void>;
+    getUserId(): Promise<string>;
 }
