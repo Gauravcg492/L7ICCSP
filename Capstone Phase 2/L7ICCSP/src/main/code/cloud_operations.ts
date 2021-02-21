f6324cef6d87d76b2ab61b1367e3255be3cfc7ec
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-22 01:47:17 +0530

    Completed verify method integration.

--- Capstone Phase 2/L7ICCSP/src/main/code/cloud_operations.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cloud_operations.ts
@@ -1,30 +1,47 @@
-import { verify } from './cloud/verify'
-import { AccessStorage } from "./storage/access_storage"
+import { verify } from './utils/verifier'
 import { sha256 } from "./utils/hashes"
+import { AccessStorage } from "./storage/access_storage"
+import { AccessCloud } from './cloud/access_cloud';
+import { Authentication } from './authenticator/authentication';
+import { constants } from './utils/constants';
+import { Tree } from './tree/tree';
 // Class which handles upload and download
-class CloudOperations{
+class CloudOperations {
+
+    private id: string;
+    private configPath: string;
     //Return types can be further refined
-    constructor()
-    {
-
-    }
-    
-    private verify(storage : AccessStorage,filehash : string): Boolean
-    {
-        const obj = {
-                 id: "id2",
-                 "configPath": 'config.json'
-         };
-        const root_hash = storage.getRootHash(obj);
-        return verify(filehash,root_hash);
+    constructor(private cloudClient: AccessCloud, private authentication: Authentication, private storage: AccessStorage) { 
+        this.id = "";
+        this.configPath = constants.CONFIG_PATH;
     }
 
-    upload(): void
-    {
+    public async setUser(): Promise<void> {
+        this.id = await this.authentication.getUserId();
+    }
+
+    private async verify(filehash: string): Promise<Boolean> {
+        const rootHash = this.storage.getRootHash({
+            id: this.id,
+            configPath: this.configPath
+        });
+        return await verify(filehash, rootHash, this.cloudClient);
+    }
+
+    upload(file: string, isFolder = false, dir: string): void {
+        if ( isFolder ) {
+            this.cloudClient.putFolder(file, dir);
+        } else {
+            this.cloudClient.putFile(file, dir);
+        }
+        const rootHash = this.storage.getRootHash({
+            id: this.id,
+            configPath: this.configPath
+        });
+
 
     }
-    download(): any
-    {
+    download(): any {
 
     }
 }
