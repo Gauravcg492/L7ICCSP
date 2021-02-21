2a252a0cdb133f5a6be6396660dfe20a1ff8fd28
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-22 01:47:29 +0530

    cli updated with more usages

--- Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
@@ -2,13 +2,8 @@ import { GoogleAuth } from "./authenticator/googleAuth";
 import readline from 'readline';
 import { GoogleAccessCloud } from "./cloud/google_access_cloud";
 import { sha256 } from "./utils/hashes";
+import { MerkleTree } from './tree/merkle_tree';
 
-function f2() {
-    console.log("f2 called");
-}
-const f1 = (f2: any) => {
-    f2();
-}
 const main = async () => {
     let tester = new GoogleAuth();
     //setTimeout(function(){ alert("Hello") },15000);
@@ -23,21 +18,34 @@ const main = async () => {
         const it = rl[Symbol.asyncIterator]();
         console.log("Access this url to get required code: ", tester.getAuthUrl());
         const code = await it.next();
-        console.log("code: ", code)
-        tester.getAccessToken(code['value']);
+        console.log("code: ", code);
+        await tester.getAccessToken(code['value']);
         console.log("Token: ", tester.isToken());
     }
     rl.close();
     const access_cloud = new GoogleAccessCloud(tester.getDrive());
-    let files = await access_cloud.getDirList();
+    // Search for main files and create them if not present
+    // console.log("Search file");
+    // let files = await access_cloud.searchFile('L7ICCSP', "");
+    // if(files.length === 0) {
+    //     access_cloud.putFolder('L7ICCSP', "");
+    // }
+    // files = await access_cloud.searchFile('merkle', "");
+    // if(files.length === 0) {
+    //     access_cloud.putFolder('merkle', "");
+    // }
+    console.log("List file")
+    let files = await access_cloud.getDirList("");
     console.log(files);
     // access_cloud.getFile('downloads/', files[files.length - 1], (file: string) => {
     //     console.log("Calling callback: ", file);
     //     const hash = sha256(file);
     //     console.log("The hash is: ", hash);
     // });
-    files = await access_cloud.searchFile('merkle');
-    console.log("files: ", files)
+    const tree = new MerkleTree("", access_cloud);
+    const fileHash = sha256('/Users/ar-gaurav.cg/Downloads/Resume.docx');
+    let rootHash = await tree.addToTree(fileHash);
+    console.log(rootHash);
 }
 
 main();
