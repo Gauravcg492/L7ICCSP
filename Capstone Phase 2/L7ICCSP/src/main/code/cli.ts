e8dfc363dc4389d1b910cfb1c1dde49f2e70a0bb
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-24 23:12:05 +0530

    Demo modifications

--- Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
@@ -53,9 +53,9 @@ const main = async () => {
     const storage: AccessStorage = new LocalAccessStorage();
     const operations = new CloudOperations(access_cloud, tester, storage);
     // await operations.setUser();
-    // await operations.upload('config.json', false, constants.ROOTDIR);
+    // await operations.upload('package.json', false, constants.ROOTDIR);
     // console.log('Upload done');
-    // operations.download("C:/Users/GAURAV C G/Desktop/Work/L7ICCSP/Capstone Phase 2/L7ICCSP/temp", "config.json");
+    operations.download("C:/Users/GAURAV C G/Desktop/Work/L7ICCSP/Capstone Phase 2/L7ICCSP/temp", "package.json");
 }
 
 main();
