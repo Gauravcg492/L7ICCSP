4ac73abda7e06d9d1f96da6da285d15d8fbe14a9
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-24 23:11:28 +0530

    added return type

--- Capstone Phase 2/L7ICCSP/src/main/code/Utils/nodeParser.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/Utils/nodeParser.ts
@@ -1,5 +1,5 @@
 
-export function parseNode(filename : String){
+export function parseNode(filename : String): string[]{
     // General Structure Of a file name
     // Filename : node_hash,parent_node_hash,left_child_hash,right_child_hash,position_1,position_2
     // return as an array
