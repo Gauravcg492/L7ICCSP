1c31f91d5525150155a5b2670db5b5d13b46479d
gauravcg492 <officialgauravcg492@gmail.com>
2021-03-21 17:49:55 +0530

    Fixed upload bug

--- Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
+++ Capstone Phase 2/L7ICCSP/src/main/code/cli.ts
@@ -1,8 +1,6 @@
 import { GoogleAuth } from "./authenticator/googleAuth";
 import readline from 'readline';
 import { GoogleAccessCloud } from "./cloud/google_access_cloud";
-import { sha256 } from "./utils/hashes";
-import { MerkleTree } from './tree/merkle_tree';
 import { CloudOperations } from './cloud_operations'
 import { LocalAccessStorage } from './storage/local_access_storage';
 import { AccessStorage } from "./storage/access_storage";
@@ -52,10 +50,18 @@ const main = async () => {
     // console.log(rootHash);
     const storage: AccessStorage = new LocalAccessStorage();
     const operations = new CloudOperations(access_cloud, tester, storage);
-    // await operations.setUser();
+    await operations.setUser();
+    // await operations.upload('config.json', false, constants.ROOTDIR);
+    // await operations.upload('credentials.json', false, constants.ROOTDIR);
+    // await operations.upload('details.json', false, constants.ROOTDIR);
+    // await operations.upload('index.html', false, constants.ROOTDIR);
+    // await operations.upload('package.json', false, constants.ROOTDIR);
+    // await operations.upload('token.json', false, constants.ROOTDIR);
     // await operations.upload('tsconfig.json', false, constants.ROOTDIR);
+    // await operations.upload('yarn.lock', false, constants.ROOTDIR);
+    // await operations.upload('webpack.common.js', false, constants.ROOTDIR);
     // console.log('Upload done');
-    operations.download("C:/Users/GAURAV C G/Desktop/Work/L7ICCSP/Capstone Phase 2/L7ICCSP/temp", "tsconfig.json");
+    operations.download("C:/Users/GAURAV C G/Desktop/Work/L7ICCSP/Capstone Phase 2/L7ICCSP/temp", "config.json");
 }
 
 main();
