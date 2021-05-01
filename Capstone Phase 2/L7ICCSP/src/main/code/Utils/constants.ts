6581df85334d53b347b6c88b80407d2d9033e563
gauravcg492 <officialgauravcg492@gmail.com>
2021-05-01 14:00:56 +0530

    Deleted utils temporarily

--- Capstone Phase 2/L7ICCSP/src/main/code/Utils/constants.ts
+++
@@ -1,7 +0,0 @@
-export const constants = {
-    SEP: ',',
-    LEAF: 1,
-    ROOTDIR: 'L7ICCSP',
-    TREEDIR: 'merkle',
-    CONFIG_PATH: 'config.json'
-}
--- Capstone Phase 2/L7ICCSP/src/main/code/Utils/logger.ts
+++
@@ -1,7 +0,0 @@
-const debug = true;
-
-export const log = (...msg:any[]) => {
-    if(debug) {
-        console.log(...msg);
-    }
-}
